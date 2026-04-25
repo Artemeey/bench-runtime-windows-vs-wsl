666704f03759769b386468d8322be473c597398e
Artemeey <artemeey.ru@mail.ru>
2026-04-25 18:25:49 +0300

    fix: invariant numeric output in files-create-delete.ps1

--- files-create-delete.ps1
+++ files-create-delete.ps1
@@ -1,5 +1,4 @@
 param (
-	# true = proxy FS, false = native FS
 	[Parameter(Mandatory = $true)]
 	[bool]$Proxy
 )
@@ -14,7 +13,6 @@ if (-not [System.IO.Directory]::Exists($root)) {
 $fileCount = 10000
 $testDir = Join-Path $root ("files-create-delete-" + [System.Guid]::NewGuid().ToString())
 
-# Create temp directory for test
 New-Item -ItemType Directory -Force -Path $testDir | Out-Null
 
 $process = Get-Process -Id $PID
@@ -23,12 +21,10 @@ $sysCpuStart = $process.PrivilegedProcessorTime
 
 $timer = [System.Diagnostics.Stopwatch]::StartNew()
 
-# Create files
 for ($i = 1; $i -le $fileCount; $i++) {
 	[System.IO.File]::WriteAllText((Join-Path $testDir "file-$i.txt"), "test")
 }
 
-# Delete directory
 [System.IO.Directory]::Delete($testDir, $true)
 
 $timer.Stop()
@@ -36,7 +32,8 @@ $process.Refresh()
 
 $userCpu = ($process.UserProcessorTime - $userCpuStart).TotalSeconds
 $sysCpu = ($process.PrivilegedProcessorTime - $sysCpuStart).TotalSeconds
+$culture = [System.Globalization.CultureInfo]::InvariantCulture
 
 Write-Output ("files: {0}" -f $fileCount)
-Write-Output ("time: {0:N3} sec" -f $timer.Elapsed.TotalSeconds)
-Write-Output ("cpu: {0:N3} user + {1:N3} sys sec" -f $userCpu, $sysCpu)
+Write-Output ("time: {0} sec" -f $timer.Elapsed.TotalSeconds.ToString("F3", $culture))
+Write-Output ("cpu: {0} user + {1} sys sec" -f $userCpu.ToString("F3", $culture), $sysCpu.ToString("F3", $culture))
